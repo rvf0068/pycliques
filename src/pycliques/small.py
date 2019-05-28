e2c6935c9e930069bef6f37a5ff183c053814147
Rafael Villarroel <rvf0068@gmail.com>
2019-05-28 16:21:41 -0500

    Add an entry point
    And delete skeleton.py

--- src/pycliques/small.py
+++ src/pycliques/small.py
@@ -1,16 +1,76 @@
+import argparse
+import sys
+import logging
+from pycliques import __version__
 from pycliques.cliques import clique_graph
 from pycliques.helly import is_helly
 import networkx as nx
 import pkg_resources
 
-gr6 = pkg_resources.resource_filename('pycliques', '/data/graph6c.g6')
-gr7 = pkg_resources.resource_filename('pycliques', '/data/graph7c.g6')
-gr8 = pkg_resources.resource_filename('pycliques', '/data/graph8c.g6')
-gr9 = pkg_resources.resource_filename('pycliques', '/data/graph9c.g6')
-l6 = nx.read_graph6(gr6)
-l7 = nx.read_graph6(gr7)
-l8 = nx.read_graph6(gr8)
-l9 = nx.read_graph6(gr9)
+__author__ = "Rafael Villarroel"
+__copyright__ = "Rafael Villarroel"
+__license__ = "mit"
+
+_logger = logging.getLogger(__name__)
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args ([str]): command line parameters as list of strings
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(
+        description="Just a Fibonnaci demonstration")
+    parser.add_argument(
+        '--version',
+        action='version',
+        version='pycliques {ver}'.format(ver=__version__))
+    parser.add_argument(
+        dest="n",
+        help="order of graphs considered",
+        type=int,
+        metavar="INT")
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action='store_const',
+        const=logging.INFO)
+    parser.add_argument(
+        '-vv',
+        '--very-verbose',
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action='store_const',
+        const=logging.DEBUG)
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(level=loglevel, stream=sys.stdout,
+                        format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
+
+
+graph6c = pkg_resources.resource_filename('pycliques', '/data/graph6c.g6')
+graph7c = pkg_resources.resource_filename('pycliques', '/data/graph7c.g6')
+graph8c = pkg_resources.resource_filename('pycliques', '/data/graph8c.g6')
+graph9c = pkg_resources.resource_filename('pycliques', '/data/graph9c.g6')
+list6 = nx.read_graph6(graph6c)
+list7 = nx.read_graph6(graph7c)
+list8 = nx.read_graph6(graph8c)
+# list9 = nx.read_graph6(graph9c)
+dict_small = {6: list6, 7: list7, 8: list8}
 
 
 def is_eventually_helly(g):
@@ -21,3 +81,34 @@ def is_eventually_helly(g):
         if g is None:
             return False
     return True
+
+
+def main(args):
+    """Main entry point allowing external calls
+
+    Args:
+      args ([str]): command line parameter list
+    """
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    _logger.debug("Starting crazy calculations...")
+    calculations = {}
+    all_graphs = dict_small[args.n]
+    total = len(all_graphs)
+    _logger.info("There are {} graphs of order {}".format(total, args.n))
+    for index in range(total):
+        _logger.debug("Considering graph with index {}".format(index))
+        calculations[index] = is_eventually_helly(all_graphs[index])
+    nonhelly = [index for index, result in calculations.items() if not result]
+    print("The indices that deserve further study are {}".format(nonhelly))
+    _logger.info("Script ends here")
+
+
+def run():
+    """Entry point for console_scripts
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    run()
