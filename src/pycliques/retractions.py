214b3da616eabea0ec409f76fb277088e1f41e30
Rafael Villarroel <rvf0068@gmail.com>
2019-06-15 18:47:23 -0500

    Use only one retraction function

--- src/pycliques/retractions.py
+++ src/pycliques/retractions.py
@@ -19,7 +19,7 @@ def _extensions(large, small, mapp):
             yield (v, value)
 
 
-def retraction2(large, small):
+def retraction(large, small):
     GM = isomorphism.GraphMatcher(large, small)
     rets = GM.subgraph_isomorphisms_iter()
     try:
@@ -34,26 +34,3 @@ def retraction2(large, small):
         return ret
     except StopIteration:
         return False
-
-
-def retraction(large, small):
-    GM = isomorphism.GraphMatcher(large, small)
-    rets = GM.subgraph_isomorphisms_iter()
-    while True:
-        try:
-            ret = next(rets)
-            for v in large.nodes()-ret.keys():
-                common = set(small.nodes())
-                for w in large[v] & ret.keys():
-                    common = common & closed_neighborhood(small, ret[w])
-                    print("With v={}, w={}, the common set is {}".format(v, w, common))
-                    for value in common:
-                        print("Trying value={}".format(value))
-                        ret[v] = value
-                        print(len(ret), len(large))
-                        if len(ret) == len(large):
-                            return ret
-                        else:
-                            continue
-        except StopIteration:
-            break
