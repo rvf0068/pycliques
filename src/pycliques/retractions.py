dba4b5d47aa9031e1d92f09b8c7cc203539d4ebe
Rafael Villarroel <rvf0068@gmail.com>
2019-06-15 18:30:44 -0500

    Another version that seems to work correctly

--- src/pycliques/retractions.py
+++ src/pycliques/retractions.py
@@ -10,6 +10,33 @@ def is_map(domain, codomain, ismap):
         return True
 
 
+def _extensions(large, small, mapp):
+    for v in large.nodes()-mapp.keys():
+        common = set(small.nodes())
+        for w in large[v] & mapp.keys():
+            common = common & closed_neighborhood(small, mapp[w])
+        for value in common:
+            yield (v, value)
+
+
+def retraction2(large, small):
+    GM = isomorphism.GraphMatcher(large, small)
+    rets = GM.subgraph_isomorphisms_iter()
+    while True:
+        try:
+            ret = next(rets)
+            while len(ret) != len(large):
+                exts = _extensions(large, small, ret)
+                try:
+                    v, w = next(exts)
+                    ret[v] = w
+                except StopIteration:
+                    return False
+            return ret
+        except StopIteration:
+            break
+
+
 def retraction(large, small):
     GM = isomorphism.GraphMatcher(large, small)
     rets = GM.subgraph_isomorphisms_iter()
