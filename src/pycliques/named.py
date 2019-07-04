fc74578e0bd893f48bf2ddfe496e8c814f3d21e5
Rafael Villarroel <rvf0068@gmail.com>
2019-07-04 16:47:32 -0500

    Suspension of graph

--- src/pycliques/named.py
+++ src/pycliques/named.py
@@ -4,6 +4,23 @@ This file defines some graphs.
 """
 
 import networkx as nx
+from networkx.algorithms.operators.binary import disjoint_union
+
+
+def graph_join(g, h):
+    join = disjoint_union(g, h)
+    ne = [(v, w) for v in range(len(g)) for w in range(len(g), len(g)+len(h))]
+    join.add_edges_from(ne)
+    return join
+
+
+def graph_suspension(g):
+    return graph_join(nx.empty_graph(2), g)
+
+
+def suspension_of_cycle(n):
+    return graph_suspension(nx.cycle_graph(n))
+
 
 octa = nx.octahedral_graph()
 path5 = nx.path_graph(5)
