f4233418428deccf421249336a47c38821c4b8f7
Rafael Villarroel <rvf0068@gmail.com>
2019-07-11 18:24:28 -0500

    Add documentation

--- src/pycliques/cliques.py
+++ src/pycliques/cliques.py
@@ -1,17 +1,21 @@
 """
-This file defines the basic functions for clique graphs.
+This file defines a function for the clique graph. It is basically
+the same as ``networkx.algorithms.clique.make_max_clique_graph``, but
+includes a parameter to abort the construction in case the graph has
+too many vertices.
 """
+
 import networkx as nx
-import itertools as itert
+import itertools
 import math
 
 
-def clique_graph(g, cmax=math.inf):
+def clique_graph(graph, bound=math.inf):
     """The clique graph function
 
     Args:
-      g (networkx.classes.graph.Graph): graph
-      cmax ([int]): upper bound accepted for order of clique graph
+      graph (networkx.classes.graph.Graph): graph
+      bound ([int]): upper bound accepted for order of clique graph
 
     Returns:
       networkx.classes.graph.Graph: the clique graph of g
@@ -24,18 +28,18 @@ def clique_graph(g, cmax=math.inf):
       NodeView((frozenset({0, 1, 2}), frozenset({0, 1, 3}), frozenset({0, 2, 4}), frozenset({0, 3, 4}), frozenset({1, 2, 5}), frozenset({1, 3, 5}), frozenset({2, 4, 5}), frozenset({3, 4, 5})))
 
     """
-    ite = nx.find_cliques(g)
+    it_cliques = nx.find_cliques(graph)
     cliques = []
     K = nx.Graph()
     while True:
         try:
-            cli = next(ite)
-            cliques.append(frozenset(cli))
-            if len(cliques) > cmax:
+            clique = next(it_cliques)
+            cliques.append(frozenset(clique))
+            if len(cliques) > bound:
                 return None
         except StopIteration:
             break
     K.add_nodes_from(cliques)
-    clique_pairs = itert.combinations(cliques, 2)
+    clique_pairs = itertools.combinations(cliques, 2)
     K.add_edges_from((c1, c2) for (c1, c2) in clique_pairs if c1 & c2)
     return K
