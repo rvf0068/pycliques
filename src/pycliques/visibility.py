d07cd12585825d02efce5aad7cd1cbc0b961ed75
Rafael Villarroel <rvf0068@gmail.com>
2020-10-23 16:56:49 -0500

    Add function for time series visibility graph

---
+++ src/pycliques/visibility.py
@@ -0,0 +1,20 @@
+from networkx import nx
+import itertools
+
+
+def is_visible(y, a, b):
+    isit = True
+    c = a + 1
+    while isit and c < b:
+        isit = y[c] < y[b]+(y[a]-y[b])*((b-c)/float(b-a))
+        c = c + 1
+    return isit
+
+
+def visibility_graph(time_series):
+    V = nx.Graph()
+    n = len(time_series)
+    pairs = itertools.combinations(range(n), 2)
+    V.add_edges_from((a, b) for (a, b) in pairs
+                     if is_visible(time_series, a, b))
+    return V
