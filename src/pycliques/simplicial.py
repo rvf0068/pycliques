699b5d242ec5d1e75d64695dc510201f93981e2e
Rafael Villarroel <rvf0068@gmail.com>
2020-10-23 16:57:25 -0500

    Small work on simplicial complexes

--- src/pycliques/simplicial.py
+++ src/pycliques/simplicial.py
@@ -5,13 +5,13 @@ class SimplicialComplex(object):
     """Documentation for SimplicialComplex
 
     """
-    def __init__(self, vertices, facet_list=[], function=None):
-        self.vertices = vertices
+    def __init__(self, vertex_set, facet_list=[], function=None):
+        self.vertex_set = set(vertex_set)
         self.facet_list = facet_list
         self.function = function
 
     def __repr__(self):
-        return f"Complex with vertices {self.vertices} and facets {self.facet_list}"
+        return f"Complex with vertex_set {self.vertex_set} and facets {self.facet_list}"
 
     def dimension(self):
         d = 0
@@ -20,7 +20,12 @@ class SimplicialComplex(object):
                 d = len(facet)
         return d
 
+    def deletion(self, x):
+        new_vertices = self.vertex_set - {x}
+        if self.function:
+            return SimplicialComplex(new_vertices, self.function)
+
 
 def clique_complex(graph):
     the_cliques = list(nx.find_cliques(graph))
-    return SimplicialComplex(graph.nodes(), the_cliques)
+    return SimplicialComplex(graph.nodes(), facet_list=the_cliques)
