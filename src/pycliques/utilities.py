153e46695a0bd67d767b88faf6975b5363487b91
Rafael Villarroel <rvf0068@gmail.com>
2020-12-02 22:52:16 -0600

    Utility to extract graphs from a g6 file
    Given the indices of the graphs to extract

--- src/pycliques/utilities.py
+++ src/pycliques/utilities.py
@@ -1,3 +1,5 @@
+import argparse
+import sys
 import gzip
 
 import networkx as nx
@@ -30,3 +32,62 @@ def graph_from_gap_adjacency_list(the_list):
     for i, adj in enumerate(the_list):
         graph.add_edges_from([(i, v-1) for v in adj])
     return graph
+
+
+def _parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args ([str]): command line parameters as list of strings
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(
+        description="Extract graphs from g6 file. Usage: extract_graphs -ff 'file.g6.gz' -l 2 10 200 -tf 'outfile.g6'")
+    parser.add_argument(
+        '-ff',
+        '--from-file',
+        dest="from_file",
+        help="From file",
+        type=str)
+    parser.add_argument(
+        '-l',
+        '--list',
+        nargs='+',
+        dest="the_list",
+        type=int,
+        help="List of graphs")
+    parser.add_argument(
+        '-tf',
+        '--to-file',
+        dest="the_file",
+        help="To file",
+        type=str)
+    return parser.parse_args(args)
+
+
+def extract_graphs_from_file(from_file, the_list, the_file):
+    index = 0
+    with gzip.open(from_file, 'rt') as graph_file:
+        with open(the_file, 'w') as extracted_graphs:
+            for graph in graph_file:
+                if index in the_list:
+                    extracted_graphs.write(graph)
+                index = index+1
+    print(the_list)
+
+
+def _main(args):
+    args = _parse_args(args)
+    extract_graphs_from_file(args.from_file, args.the_list, args.the_file)
+
+
+def _run():
+    """Entry point for console_scripts
+    """
+    _main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    _run()
