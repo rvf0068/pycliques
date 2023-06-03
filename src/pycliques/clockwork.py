b4f8fd21a180b7bf65eda2bfc398d3ae2a15d20d
Rafael Villarroel <rvf0068@gmail.com>
2023-06-03 04:33:50 +0000

    Fix some problems

--- src/pycliques/clockwork.py
+++ src/pycliques/clockwork.py
@@ -1,11 +1,13 @@
-import networkx as nx
+"""Module for the construction of clockwork graphs."""
+
 import itertools
+import networkx as nx
 
 
 def core(segments, neig_segments):
     """Returns the core graph with segments """
-    core = nx.Graph()
-    core.add_nodes_from(range(sum(segments)))
+    core_graph = nx.Graph()
+    core_graph.add_nodes_from(range(sum(segments)))
     segment_list = []
     for seg in range(len(segments)):
         segment_list.append(list(range(sum(segments[:seg]),
@@ -17,16 +19,17 @@ def core(segments, neig_segments):
             else:
                 start_next = 0
             for j in range(neig_segments[seg][i]):
-                core.add_edge(curr_vertex, start_next+j)
+                core_graph.add_edge(curr_vertex, start_next+j)
     for segment in segment_list:
         pairs = itertools.combinations(segment, 2)
-        core.add_edges_from(pairs)
-    return core, segment_list
+        core_graph.add_edges_from(pairs)
+    return core_graph, segment_list
 
 
 def crown(num_segments, size_segments, permutation):
-    crown = nx.Graph()
-    crown.add_nodes_from(range(num_segments * size_segments))
+    """Returns the crown graph with given segments and permutation"""
+    crown_graph = nx.Graph()
+    crown_graph.add_nodes_from(range(num_segments * size_segments))
     for seg in range(num_segments):
         for i in range(size_segments):
             curr_vertex = size_segments*seg + i
@@ -34,35 +37,37 @@ def crown(num_segments, size_segments, permutation):
                 end_vertex = size_segments*(seg+1) + i
             else:
                 end_vertex = permutation[i]
-            crown.add_edge(curr_vertex, end_vertex)
+            crown_graph.add_edge(curr_vertex, end_vertex)
     segment_list = [list(range(size_segments*i, size_segments*(i+1)))
                     for i in range(num_segments)]
     for segment in segment_list:
         pairs = itertools.combinations(segment, 2)
-        crown.add_edges_from(pairs)
-    return crown, segment_list
+        crown_graph.add_edges_from(pairs)
+    return crown_graph, segment_list
 
 
 def segmented_sum(segmented_b, segmented_c):
+    """Returns the segmented sum of two segmented graphs"""
     graph_b = segmented_b[0]
     graph_c = segmented_c[0]
     segments_b = segmented_b[1]
     segments_c = segmented_c[1]
-    segmented_sum = nx.disjoint_union(graph_b, graph_c)
+    segmented_sum_graph = nx.disjoint_union(graph_b, graph_c)
     new_segments_c = [[x+graph_b.order() for x in seg] for seg in segments_c]
     for seg in range(len(segments_c)):
-        segmented_sum.add_edges_from([(u, v) for u in segments_b[seg]
+        segmented_sum_graph.add_edges_from([(u, v) for u in segments_b[seg]
                                       for v in new_segments_c[seg]])
         if seg == len(segments_c)-1:
             next_seg = 0
         else:
             next_seg = seg+1
-        segmented_sum.add_edges_from([(u, v) for u in segments_b[next_seg]
+        segmented_sum_graph.add_edges_from([(u, v) for u in segments_b[next_seg]
                                       for v in new_segments_c[seg]])
-    return segmented_sum
+    return segmented_sum_graph
 
 
 def clockwork_graph(segments, neig_segments, size_seg_crown, permutation):
+    """Returns the clockwork graph with given segments and data"""
     the_core = core(segments, neig_segments)
     the_crown = crown(len(segments), size_seg_crown, permutation)
     return segmented_sum(the_crown, the_core)
